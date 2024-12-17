5da1adad3a8318d1d8cfb35bfc85f7a286e2ce1c
Wouter Deconinck <wdconinc@gmail.com>
2024-12-17 12:08:12 -0600

    root: only depends_on fortran when +fortran (#48122)

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -93,9 +93,9 @@ class Root(CMakePackage):
     version("6.06.04", sha256="ab86dcc80cbd8e704099af0789e23f49469932ac4936d2291602301a7aa8795b")
     version("6.06.02", sha256="18a4ce42ee19e1a810d5351f74ec9550e6e422b13b5c58e0c3db740cdbc569d1")
 
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
-    depends_on("fortran", type="build")  # generated
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build", when="+fortran")
 
     # ###################### Patches ##########################
 
